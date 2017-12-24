a3c02036c2f30640c11565390339852d0fc27e32
Petar Nikolov <penikolov23@gmail.com>
2017-12-24 23:49:58 +0200

    Split code into seperate files

---
+++ code/arnoldcpm.rb
@@ -0,0 +1,33 @@
+require_relative 'arnoldcpm/interpreter'
+
+module ArnoldCPM
+  extend Interpreter
+
+  class << self
+    def method_missing(name, *args, &block)
+      # Return name if this is an ArnoldC variable or function name
+      return name if args.empty? && !block_given?
+      super(name, *args, &block)
+    end
+
+    def printer
+      @@printer
+    end
+
+    def printer=(printer)
+      @@printer = printer 
+    end
+
+    def totally_recall(&code)
+      initialize_program
+      instance_eval(&code)
+      execute_program
+      reset
+    end
+
+    private def reset
+      reset_interpreter
+    end
+  end
+end
+
---
+++ code/execute.rb
@@ -0,0 +1,9 @@
+require_relative 'arnoldcpm'
+
+ArnoldCPM.printer = Kernel
+ArnoldCPM.totally_recall do
+  its_showtime
+    talk_to_the_hand 42
+  you_have_been_terminated
+end
+
